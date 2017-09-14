80c035af26d0bf66a7b2ad7892f28d36b1b73ad9
Leandro Alves <leandrosustenido@gmail.com>
2017-09-14 18:14:14 -0300

    fix: attempt infinit loop

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -29,7 +29,7 @@ class AttemptsController < ApplicationController
       correct_options_text = @survey.correct_options.present? ? 'Bellow are the correct answers marked in green' : ''
       redirect_to attempt_path(@attempt.id), notice: "Thank you for answering #{@survey.name}! #{correct_options_text}"
     else
-      build_flash(@attempt)   
+      build_flash(@attempt)
       @participant = current_user
       render :new
     end
@@ -51,9 +51,5 @@ class AttemptsController < ApplicationController
       params[:survey_attempt][:answers_attributes] = params[:survey_attempt][:answers_attributes].map { |attrs| { question_id: attrs.first, option_id: attrs.last } }
       params.require(:survey_attempt).permit(Survey::Attempt::AccessibleAttributes)
     end
-  end
-
-  def current_user
-    view_context.current_user
   end
 end
