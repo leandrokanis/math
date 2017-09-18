8ad67ecb56fa61d4cf6ea96fb4d224d8c6ecc192
Leandro Alves <leandrosustenido@gmail.com>
2017-09-18 17:53:56 -0300

    Creating visitors home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'surveys#index'
+  root 'welcome#index'
   resources 'surveys'
   resources 'attempts'
   # resources 'users', only: [:create]
