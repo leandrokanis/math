0f92dd76eef491d9e30ad28e5c37727b4ba95e6b
Leandro Alves <leandrosustenido@gmail.com>
2017-09-19 15:13:39 -0300

    login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   #validates :name, length: { maximum: 30 }, presence: true
+  attr_accessor :login
   has_surveys
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
